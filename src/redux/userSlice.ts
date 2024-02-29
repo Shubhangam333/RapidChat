fa7502dd26c719c7ebc8259eaefa9484453ff891
Shubhangam333 <shubhangam27@gmail.com>
2024-02-29 15:56:24 +0530

    setting up redux oolkit

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    currentUserData: null,
+    currentUserId: "",
+  },
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUserData = action.payload;
+    },
+    setCurrentUserId: (state, action) => {
+      state.currentUserId = action.payload;
+    },
+  },
+});
+
+export const { setCurrentUser, setCurrentUserId } = userSlice.actions;
+
+export default userSlice;
