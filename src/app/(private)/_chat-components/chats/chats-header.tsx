7c8416c64a554ce5f4852251d382c08e6d0ee446
Shubhangam333 <shubhangam27@gmail.com>
2024-03-02 15:51:42 +0530

    create model and interface of chat and message

---
+++ src/app/(private)/_chat-components/chats/chats-header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ChatsHeader() {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-xl text-gray-500 font-bold uppercase">My Chats</h1>
+      </div>
+    </div>
+  );
+}
+
+export default ChatsHeader;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface UserType {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface ChatType {
+  _id: string;
+  users: UserType[];
+  createdBy: UserType;
+  lastMessage: MessageType;
+  isGroupChat: boolean;
+  groupName: string;
+  groupProfilePicture: string;
+  groupBio: string;
+  groupAdmins: UserType[];
+  unreadCounts: any;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface MessageType {
+  _id: string;
+  Chat: ChatType;
+  sender: UserType;
+  text: string;
+  image: string;
+  readBy: UserType[];
+  createdAt: string;
+  updatedAt: string;
+}
